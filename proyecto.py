4e39835a71cd6d3aaa97efbbf456c8b456bfcbe4
zuniga-eduardo <zunigaeduardo307@gmail.com>
2026-04-03 00:00:23 -0600

    Update proyecto.py
    + funcion menu() estadarizar menu 
    
    Main ahora imprime menu con funcion y evita exhastivo uso de print()

--- proyecto.py
+++ proyecto.py
@@ -4,7 +4,9 @@
 # \033 es ESC inica un comando para dar formato a la salida en consola, aqui se utiliza [H y [J son parametros para el escape, se mueve el cursor a la esquina noroeste de la consola y se limpia la consola desde el cursor hasta fin de pantalla respectivamente.
 ASCII_limpiar = "\033[H\033[J"
 
-
+# Contenido de menus
+MenuPrincipal = ["Registro de usuario y carrito de compras","Cálculo de totales y validaciones de stock", "Visualización del resumen de compra", "Informes", "Salir"]
+Modulo1 = ["Registro de usuario","Carrito de compras"]
 
 # funcion para mas limpiamente limpiar pantalla sin hacer print cada ves, 'end' en el print evita que se cree una nueva linea luego de limpiar pantalla que es el comportamiento por defecto
 def limpiar_consola():
@@ -12,22 +14,25 @@ def limpiar_consola():
     return
 
 
+# Funcion para normalizar menus y evitar extensivo uso de funciones print()
+def menu(titulo, opciones, texto):
+    limpiar_consola()
+    print(f"          ---------- {titulo} ----------\n\n")
+    for i in range(opciones):
+        print(f"{i+1}. {texto[i]}\n")
+    return
 
+# Simulador de compras en linea
 def main():
 
     while True:
-        print("---------- Menu ----------\n\n")
-        print("opcion 1\n")
-        print("opcion 2\n")
-        print("opcion 3\n")
+        menu("Menu",5,MenuPrincipal)
+        
         opcion = input("seleccione una opcion: ")
         match int(opcion):
             case 1:
-                limpiar_consola()
-                print("---------- SubMenu ----------\n\n")
-                print("Nuevo menu")
+                menu("Modulo 1",2,Modulo1)
                 input()
-                limpiar_consola()
             case _:
                 return
 
